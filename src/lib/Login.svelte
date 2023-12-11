30d29266e4b0025cc0ff0dde29ccc32455f6eed2
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2023-12-11 20:03:37 +0000

    improve login UI consistency

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -14,20 +14,20 @@
 </script>
 <div class="max-w-sm px-4 w-full">
 	<div class="bg-white rounded-lg shadow-lg max-w-sm w-full flex flex-col items-center p-6">
-		<div class="text-center text-lg text-slate-800 font-medium mb-2 rounded-md">Bem vindo ao mGira!</div>
+		<div class="text-center text-lg text-neutral-800 font-medium mb-2 rounded-md">Bem vindo ao Gira+</div>
 		<form class="flex flex-col w-full gap-4" on:submit={loginWrapper}>
 			<div class="w-full">
-				<div class="text-sm text-slate-700 pb-1">Email</div>
-				<input type="email" required bind:value={email} placeholder="example@example.pt" class="w-full px-2 h-12 rounded-md border border-slate-200 bg-slate-100 focus:ring-green-500 focus:border-green-500">
+				<div class="text-sm text-neutral-700 pb-1">Email</div>
+				<input type="email" required bind:value={email} placeholder="example@example.pt" class="w-full px-2 h-12 rounded-md border border-neutral-200 bg-neutral-100 focus:ring-primary focus:border-primary">
 			</div>
 			<div class="w-full">
-				<div class="text-sm text-slate-700 pb-1">Password</div>
-				<input type="password" required bind:value={password} class="w-full px-2 h-12 rounded-md border border-slate-200 bg-slate-100 focus:ring-green-500 focus:border-green-500">
+				<div class="text-sm text-neutral-700 pb-1">Password</div>
+				<input type="password" required bind:value={password} class="w-full px-2 h-12 rounded-md border border-neutral-200 bg-neutral-100 focus:ring-primary focus:border-primary">
 				{#if errorCode !== null && errorCode !== 0}
 					<div class="text-sm text-red-500 pt-1">{errorCodes[errorCode] || 'Erro desconhecido'}</div>
 				{/if}
 			</div>
-			<button class="form-textarea border-0  w-full h-12 rounded-md bg-green-400 text-white font-semibold font-mono focus:ring-green-500 focus:border-green-500 ">Login</button>
+			<button class="form-textarea border-0  w-full h-12 rounded-md bg-primary text-white font-bold focus:ring-primary focus:border-primary ">Login</button>
 		</form>
 	</div>
 </div>
