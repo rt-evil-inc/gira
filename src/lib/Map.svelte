84d3c0b61f054ed805c161ec58c00f5b4719c0ab
ttmx <git@tteles.dev>
2023-12-13 02:05:01 +0000

    Add attribution control to the map and refactor code

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -1,6 +1,6 @@
  <script lang="ts">
 	import { createEventDispatcher, onMount, tick } from 'svelte';
-	import { GeoJSONSource, Map } from 'maplibre-gl';
+	import { AttributionControl, GeoJSONSource, Map } from 'maplibre-gl';
 	import { stations } from '$lib/stores';
 	// import GeoJSON
 	import type { GeoJSON } from 'geojson';
@@ -33,69 +33,82 @@
 				})),
 			};
 			src.setData(data);
+		} else {
+			map.addSource('points', {
+				'type': 'geojson',
+				'data': { type: 'FeatureCollection', features: [] },
+			});
 		}
 	}
+	function loadSvg(url: string): Promise<HTMLImageElement> {
+		return new Promise((resolve, reject) => {
+			let img = new Image;
+			img.onload = _ => resolve(img);
+			img.onerror = reject;
+			img.src = url;
+		});
+	}
+
+	function addLayers() {
+		map.addLayer({
+			'id': 'points',
+			'type': 'symbol',
+			'source': 'points',
+			'layout': {
+				// bike_white if active, bike_green otherwise
+				'icon-image': ['case', ['get', 'active'], 'bike_green', 'bike_white'],
+				'icon-size': 0.5,
+				'icon-anchor': 'bottom',
+				'text-field': '{bikes}',
+				'text-size': 20,
+				'text-offset': [0, -2],
+				'text-font': ['Noto Sans Bold'],
+			},
+			paint: {
+				'text-color': ['case', ['get', 'active'], '#fff', '#79c000'],
+			},
+		});
+	}
+
+	function addEventListeners() {
+		map.on('click', 'points', async function (e) {
+			if (e.features === undefined) return;
+			const feature = e.features[0] as GeoJSON.Feature<GeoJSON.Point>;
+			const props = feature.properties as { serialNumber: string, name: string, bikes: number };
+			selectedStation = props.serialNumber;
+			await tick();
+			await tick();
+			map.flyTo({
+				center: feature.geometry.coordinates as [number, number],
+				padding: { bottom: Math.min(menuHeight, window.innerHeight / 2) },
+			});
+		});
+	}
+	function loadImages() {
+		const imgs = [['bike_white', './assets/bike_marker_white.svg'], ['bike_green', './assets/bike_marker_green.svg']];
+		return Promise.all(imgs.map(([name, url]) => loadSvg(url).then(img => {
+			map.addImage(name, img);
+		})));
+	}
+
+	async function onMapLoad() {
+		mapLoaded = true;
+		await loadImages();
+		addLayers();
+		addEventListeners();
+		setSourceData();
+	}
+
 	onMount(async () => {
 		map = new Map({
 			container: mapElem,
 			style: 'https://tiles2.intermodal.pt/styles/iml/style.json',
 			center: [-9.15, 38.744],
 			zoom: 11,
+			attributionControl: false,
 		});
-		map.on('load', () => {
-			mapLoaded = true;
-			let bike_white = new Image;
-			let promiseWhite = new Promise((resolve, reject) => {
-				bike_white.onload = e => { map.addImage('bike_white', bike_white); resolve(e); };
-				bike_white.onerror = reject;
-				bike_white.src = './assets/bike_marker_white.svg';
-			});
-			let bike_green = new Image;
-			let promiseGreen = new Promise((resolve, reject) => {
-				bike_green.onload = e => { map.addImage('bike_green', bike_green); resolve(e); };
-				bike_green.onerror = reject;
-				bike_green.src = './assets/bike_marker_green.svg';
-			});
-			Promise.all([promiseGreen, promiseWhite]).then(
-				() => {
-					map.addLayer({
-						'id': 'points',
-						'type': 'symbol',
-						'source': 'points',
-						'layout': {
-							// bike_white if active, bike_green otherwise
-							'icon-image': ['case', ['get', 'active'], 'bike_green', 'bike_white'],
-							'icon-size': 0.5,
-							'icon-anchor': 'bottom',
-							'text-field': '{bikes}',
-							'text-size': 20,
-							'text-offset': [0, -2],
-							'text-font': ['Noto Sans Bold'],
-						},
-						paint: {
-							'text-color': ['case', ['get', 'active'], '#fff', '#79c000'],
-						},
-					});
-					map.on('click', 'points', async function (e) {
-						if (e.features === undefined) return;
-						const feature = e.features[0] as GeoJSON.Feature<GeoJSON.Point>;
-						const props = feature.properties as { serialNumber: string, name: string, bikes: number };
-						selectedStation = props.serialNumber;
-						await tick();
-						await tick();
-						map.flyTo({
-							center: feature.geometry.coordinates as [number, number],
-							padding: { bottom: Math.min(menuHeight, window.innerHeight / 2) },
-						});
-					});
-				},
-			);
-			map.addSource('points', {
-				'type': 'geojson',
-				'data': { type: 'FeatureCollection', features: [] },
-			});
-			setSourceData();
-		});
+		map.addControl(new AttributionControl, 'bottom-left');
+		map.on('load', onMapLoad);
 	});
 	$:if ($stations && map) {
 		selectedStation = selectedStation;
