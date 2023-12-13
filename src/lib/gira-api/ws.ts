1d322d4683ebfb0936e3ef5c2d93125d76951f84
ttmx <git@tteles.dev>
2023-12-13 04:25:20 +0000

    Fix Websockets on mobile

--- src/lib/gira-api/ws.ts
+++ src/lib/gira-api/ws.ts
@@ -2,6 +2,14 @@ import { stations, token } from '$lib/stores';
 import { get } from 'svelte/store';
 import type { WSEvent } from './ws-types';
 let ws: WebSocket;
+
+function randomUUID() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+		const r = Math.random() * 16 | 0, v = c == 'x' ? r : r & 0x3 | 0x8;
+		return v.toString(16);
+	});
+}
+
 export function startWS() {
 	const tokens = get(token);
 	const access = tokens?.accessToken;
@@ -16,7 +24,7 @@ export function startWS() {
 		ws.send(JSON.stringify({ 'type': 'connection_init' }));
 		ws.send(JSON.stringify({
 			'type': 'start',
-			'id': crypto.randomUUID(),
+			'id': randomUUID(),
 			'payload': {
 				'operationName': 'activeTripSubscription',
 				'query': 'subscription activeTripSubscription($_access_token: String) { activeTripSubscription(_access_token: $_access_token) { code bike startDate endDate cost finished canPayWithMoney canUsePoints clientPoints tripPoints canceled period periodTime error}}',
@@ -27,7 +35,7 @@ export function startWS() {
 		}));
 		ws.send(JSON.stringify({
 			'type': 'start',
-			'id': crypto.randomUUID(),
+			'id': randomUUID(),
 			'payload': {
 				'operationName': 'operationalStationsSubscription',
 				'query': 'subscription operationalStationsSubscription {operationalStationsSubscription {assetCondition, assetStatus, assetType, code, description, latitude, longitude, name, bikes, docks, serialNumber, stype}}',
