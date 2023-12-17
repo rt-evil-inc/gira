deea15adf3290292e8f60c3c59a1e1976fac539d
ttmx <git@tteles.dev>
2023-12-17 01:51:07 +0000

    More settings progress

--- src/lib/components/SettingsEntry.svelte
+++ src/lib/components/SettingsEntry.svelte
@@ -5,7 +5,7 @@
 	export let icon: typeof SvelteComponent;
 	export let subtext = '';
 </script>
-<div class="flex gap-2 items-center bg-white p-2 rounded-2xl" >
+<button class="flex text-left gap-2 items-center bg-white p-2 rounded-2xl active:bg-neutral-100 transition-all" on:click>
 	{#if icon}
 		<svelte:component this={icon} class="text-neutral-700" size={24} />
 	{/if}
@@ -13,4 +13,4 @@
 		<span class="text-md font-semibold text-neutral-700">{text}</span>
 		<span class="text-xs text-neutral-500">{subtext}</span>
 	</div>
-</div>
+</button>
