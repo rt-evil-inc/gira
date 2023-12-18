07b6f75d7fbc6f5f3912100f6ef2ce3aedc27922
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2023-12-18 13:28:01 +0000

    fix color consistency on bike skeletons

--- src/lib/components/Bike.svelte
+++ src/lib/components/Bike.svelte
@@ -115,14 +115,14 @@
 		</div>
 	{:else}
 		<div class="absolute flex items-center bg-background rounded-2xl h-full w-full px-5 gap-5" style:box-shadow="0px 0px 12px 0px var(--color-shadow)" >
-			<div class="w-[36px] h-[36px] -ml-2 -mr-2 bg-gray-200 animate-pulse rounded-lg"></div>
-			<span class="h-6 font-semibold bg-gray-200 rounded-lg w-16 animate-pulse"></span>
+			<div class="w-[36px] h-[36px] -ml-2 -mr-2 bg-background-tertiary animate-pulse rounded-lg"></div>
+			<span class="h-6 font-semibold bg-background-tertiary rounded-lg w-16 animate-pulse"></span>
 			<div class="grow" />
 			{#if Math.random() < 0.5}
-				<div class="flex items-center h-6 px-[6px] bg-gray animate-pulse rounded-md gap-1 w-16 bg-gray-200">
+				<div class="flex items-center h-6 px-[6px] animate-pulse rounded-md gap-1 w-16 bg-background-tertiary">
 				</div>
 			{/if}
-			<div class="flex flex-col items-center text-primary w-6 h-8 bg-gray-200 animate-pulse rounded-lg">
+			<div class="flex flex-col items-center text-primary w-6 h-8 bg-background-tertiary animate-pulse rounded-lg">
 			</div>
 		</div>
 	{/if}
