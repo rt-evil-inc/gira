c5be977b0c5a6c5eb9adecfdf7e12666bad26d49
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2023-12-21 21:23:22 +0000

    fix missing href change

--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -65,7 +65,7 @@
 			<div class="flex flex-col grow font-semibold px-2 gap-3 w-full">
 				<SettingsEntry icon={IconHistory} text={'Histórico'} subtext={'Lista de viagens anteriores'} on:click={() => openPage = 'history'} />
 				<SettingsEntry icon={IconTool} text={'Configurações'} subtext={'Definições da aplicação'} on:click={() => openPage = 'settings'} />
-				<a href="https://github.com/ttmx/gira-app/issues"><SettingsEntry icon={IconMessageReport} text={'Feedback'} subtext={'Problemas e sugestões'} /></a>
+				<a href="https://github.com/rt-evil-inc/gira-plus/issues"><SettingsEntry icon={IconMessageReport} text={'Feedback'} subtext={'Problemas e sugestões'} /></a>
 				<SettingsEntry icon={IconInfoCircle} text={'Sobre'} subtext={'Informação acerca da aplicação'} on:click={() => openPage = 'info'} />
 			</div>
 			<button class="flex flex-col items-center mb-3" on:click={() => { dispatch('close'); logOut(); }}>
