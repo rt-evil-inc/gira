56370b0f1ad1f9c7ad39c251db638a20bec19805
ttmx <git@tteles.dev>
2023-12-21 12:59:10 +0000

    Basic history skeleton

--- src/lib/components/settings/History.svelte
+++ src/lib/components/settings/History.svelte
@@ -5,11 +5,13 @@
 	import HistoryItem from './HistoryItem.svelte';
 	let trips:TripHistory_TripDetail[] = [];
 	let observed:HTMLDivElement;
+	let didFirstRequest = false;
 	let loading = false;
 	function loadMoreTripHistory() {
 		if (loading) return;
 		loading = true;
 		getTripHistory(Math.floor(trips.length / 15), 15).then(res => {
+			didFirstRequest = true;
 			if (res.tripHistory == null) return;
 			trips = trips.concat(res.tripHistory.reduce((acc, cur) => {
 				if (cur != null) acc.push(cur);
@@ -42,6 +44,7 @@
 
 	$: aggregate = Object.entries(trips.reduce((acc, cur) => {
 		if (cur == null) return acc;
+		console.log('cur', cur);
 		const date = new Date(cur.startDate);
 		const key = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
 		if (acc[key] == null) acc[key] = [date.getTime(), []];
@@ -59,17 +62,38 @@
 	<div class="flex flex-col h-full overflow-y-auto">
 		<div class="fixed left-0 right-0 h-4 -mt-4" style:box-shadow="0px 6px 6px 0px var(--color-background)" />
 		<div class="text-2xl font-bold text-info pl-4 pt-1">Viagens</div>
-
-		<div class="flex flex-col gap-2 p-4">
-			{#each aggregate as [_, tripsObj]}
-				<div class="flex flex-col gap-2">
-					<div class="font-semibold text-label text-sm">{formatDate(new Date(parseInt(tripsObj[0])))}</div>
-					{#each tripsObj[1] as trip}
-						<HistoryItem trip={trip} />
-					{/each}
+		{#if didFirstRequest }
+			{#if didFirstRequest && trips.length == 0 }
+				<div class="flex flex-col items-center justify-center h-full">
+					<div class="text-2xl font-bold text-info">Nenhuma viagem</div>
+					<div class="text-label text-sm text-center">Não há viagens registadas</div>
 				</div>
-			{/each}
-			<div bind:this={observed}></div>
-		</div>
+			{:else}
+				<div class="flex flex-col gap-2 p-4">
+					{#each aggregate as [_, tripsObj]}
+						<div class="flex flex-col gap-2">
+							<div class="font-semibold text-label text-sm">{formatDate(new Date(tripsObj[0]))}</div>
+							{#each tripsObj[1] as trip}
+								<HistoryItem trip={trip} />
+							{/each}
+						</div>
+					{/each}
+					<div bind:this={observed}></div>
+				</div>
+			{/if}
+		{:else}
+			<div class="flex flex-col gap-2 p-4">
+				{#each new Array(4).fill(0) as _}
+					<div class="flex flex-col gap-2">
+						<div class="font-semibold text-label text-sm h-4 bg-neutral-100 rounded-xl animate-pulse"
+							style:width={(Math.random() * 15 + 40) + '%'}
+						></div>
+						{#each new Array(Math.floor(Math.random() * 3 + 1)).fill(0) as _}
+							<HistoryItem trip={null} />
+						{/each}
+					</div>
+				{/each}
+			</div>
+		{/if}
 	</div>
 </div>
