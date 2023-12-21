ca029e5844d5f1679f6edb8fcf982b4a5a110ce1
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2023-12-21 21:11:47 +0000

    fix number localization

--- src/lib/components/Metric.svelte
+++ src/lib/components/Metric.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex flex-col items-center">
 	<div>
-		<span class="text-2xl font-bold text-{color} pr-px">{value}</span><span class="text-sm font-semibold text-label">
+		<span class="text-2xl font-bold text-{color} pr-px">{value.toLocaleString(undefined, { maximumFractionDigits: 2, useGrouping: false })}</span><span class="text-sm font-semibold text-label">
 			{unit}
 		</span>
 	</div>
