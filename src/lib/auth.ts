5171e810dc9cea08c4c6cb670d5684453c958a48
ttmx <git@tteles.dev>
2023-12-24 02:31:24 +0000

    Login with pw and mail if token refresh fails

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { get } from 'svelte/store';
 import { getTokensLogin, getTokensRefresh, getUserInfo } from './emel-api/emel-api';
-import { token, user } from './stores';
+import { token, user, userCredentials } from './stores';
 
 export async function login(email: string, password: string) {
 	const response = await getTokensLogin(email, password);
@@ -12,11 +12,12 @@ export async function login(email: string, password: string) {
 }
 
 const msBetweenRefreshAttempts = 2000;
+const attempts = 5;
 export async function refreshToken() {
 	const tokens = get(token);
-	if (!tokens) return;
+	if (!tokens) return false;
 	let success = false;
-	while (!success) {
+	for (let i = 0; i < attempts && !success; i++) {
 		const response = await getTokensRefresh(tokens);
 		if (response.error.code !== 0 || !response.data.accessToken || !response.data.refreshToken) {
 			await new Promise(resolve => setTimeout(resolve, msBetweenRefreshAttempts));
@@ -25,6 +26,21 @@ export async function refreshToken() {
 		const { accessToken, refreshToken, expiration } = response.data;
 		token.set({ accessToken, refreshToken, expiration });
 		success = true;
+	}
+	if (!success) {
+		const success = false;
+		for (let i = 0; i < attempts && !success; i++) {
+			const creds = get(userCredentials);
+			if (!creds) return false;
+			const res = await login(creds.email, creds.password);
+			if (res !== 0) {
+				await new Promise(resolve => setTimeout(resolve, msBetweenRefreshAttempts));
+				continue;
+			} else {
+				break;
+			}
+		}
+		return success;
 	}
 	return true;
 }
