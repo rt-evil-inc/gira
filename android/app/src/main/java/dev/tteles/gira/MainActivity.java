6709af1527c4dc5c848dd9dfb676bf35ff393378
ttmx <git@tteles.dev>
2023-12-26 11:40:05 +0000

    Fix keyboard events not firing

--- android/app/src/main/java/dev/tteles/gira/MainActivity.java
+++ android/app/src/main/java/dev/tteles/gira/MainActivity.java
@@ -1,14 +1,17 @@
 package dev.tteles.gira;
 
+
 import com.getcapacitor.BridgeActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.view.View;
+
 
 public class MainActivity extends BridgeActivity {
 
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
   }
 }
