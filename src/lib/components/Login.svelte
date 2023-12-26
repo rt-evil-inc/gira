3756f06a670f8112da4d0275ff1e566ef2d0ef8a
ttmx <git@tteles.dev>
2023-12-26 11:46:11 +0000

    Clear error in login when user input

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -7,11 +7,14 @@
 	let email = '';
 	let password = '';
 	let errorCode:number|null = null;
-	let errorCodes:{[key:number]:string} = {
+	const errorCodes:{[key:number]:string} = {
 		100: 'Credenciais inválidas',
 	};
-
+	function clearError() {
+		errorCode = null;
+	}
 	async function loginWrapper() {
+		errorCode = null;
 		errorCode = await login(email, password);
 		if (errorCode === 0) userCredentials.set({ email, password });
 		else console.log(errorCode);
@@ -32,11 +35,11 @@
 		<form class="flex flex-col w-full gap-4" on:submit={loginWrapper}>
 			<div class="w-full">
 				<div class="text-sm text-info font-medium pb-1">Email</div>
-				<input type="email" required bind:value={email} placeholder="exemplo@exemplo.pt" class="placeholder-label w-full px-2 h-12 rounded-lg border border-background-tertiary bg-background-secondary focus:ring-primary focus:border-primary">
+				<input type="email" required on:input={clearError} bind:value={email} placeholder="exemplo@exemplo.pt" class="placeholder-label w-full px-2 h-12 rounded-lg border border-background-tertiary bg-background-secondary focus:ring-primary focus:border-primary">
 			</div>
 			<div class="w-full">
 				<div class="text-sm text-info font-medium pb-1">Password</div>
-				<input type="password" required bind:value={password} placeholder="••••••••" class="placeholder-label w-full px-2 h-12 rounded-lg border border-background-tertiary bg-background-secondary focus:ring-primary focus:border-primary">
+				<input type="password" required on:input={clearError} bind:value={password} placeholder="••••••••" class="placeholder-label w-full px-2 h-12 rounded-lg border border-background-tertiary bg-background-secondary focus:ring-primary focus:border-primary">
 				{#if errorCode !== null && errorCode !== 0}
 					<div class="text-sm text-red-500 pt-1">{errorCodes[errorCode] || 'Erro desconhecido'}</div>
 				{/if}
