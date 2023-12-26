28b7c3e6a373b0d698412b76bd64afdf15336a52
ttmx <git@tteles.dev>
2023-12-26 05:02:00 +0000

    Make settings string depend on lock distance constant

--- src/lib/components/settings/Settings.svelte
+++ src/lib/components/settings/Settings.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { LOCK_DISTANCE_m } from '$lib/constants';
 	import { appSettings } from '$lib/stores';
 	import MenuPage from '../MenuPage.svelte';
 	import Toggle from '../Toggle.svelte';
@@ -11,7 +12,7 @@
 			<div class="flex bg-white rounded-2xl py-4 px-5 gap-5 text-info justify-between items-center" style:box-shadow="0px 0px 12px 0px var(--color-shadow)">
 				<div class="grow">
 					<div class="font-semibold leading-relaxed">Limitar desbloqueio</div>
-					<div class="text-xs font-medium leading-[1.1] text-label">Restringir a distância de desbloqueio de bicicletas a 35m</div>
+					<div class="text-xs font-medium leading-[1.1] text-label">Restringir a distância de desbloqueio de bicicletas a {LOCK_DISTANCE_m}m</div>
 				</div>
 				<Toggle bind:checked={$appSettings.distanceLock} />
 			</div>
