868e723ca2a009ae3fe2d7dd93d210a1f2dc3601
ttmx <git@tteles.dev>
2024-01-06 01:10:10 +0000

    Attempt release action

--- misc/release/release-android.sh
+++ misc/release/release-android.sh
@@ -3,10 +3,8 @@ INPUTS=android/app/build/outputs
 bun run build
 bunx cap sync android
 cd android
-./gradlew assembleRelease
-./gradlew bundleRelease
+./gradlew assembleRelease bundleRelease
 cd ..
 mkdir -p misc/outputs
-cp $INPUTS/apk/release/app-release-unsigned.apk misc/outputs/gira.apk
-apksigner sign --ks misc/release/android-release.keystore --ks-key-alias release misc/outputs/gira.apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore misc/release/android-release.keystore -signedjar misc/outputs/gira-signed.aab $INPUTS/bundle/release/app-release.aab release
+cp $INPUTS/apk/release/app-release.apk misc/outputs/gira.apk
+cp $INPUTS/bundle/release/app-release.aab misc/outputs/gira.aab
