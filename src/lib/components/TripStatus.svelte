37b293a1a33c69d04c365e887770bc1f3fdef998
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2024-09-22 02:39:38 +0100

    refactor trip timer update logic in attempt to fix timer duplication bug

--- src/lib/components/TripStatus.svelte
+++ src/lib/components/TripStatus.svelte
@@ -21,13 +21,12 @@
 		ScreenOrientation.lock({ orientation: 'portrait' });
 	}
 
-	let seconds = 0;
+	let seconds: number;
 	let inter: ReturnType<typeof setInterval>;
 
 	onMount(() => {
 		seconds = 0;
-		let startTime = Date.now();
-		inter = setInterval(() => seconds = (Date.now() - startTime) / 1000, 1000);
+		inter = setInterval(() => seconds++, 1000);
 		$following = true;
 		KeepAwake.keepAwake();
 		ScreenOrientation.orientation().then(o => portrait = o.type === 'portrait-primary');
@@ -52,41 +51,39 @@
 </script>
 
 <div transition:fly={portrait ? { y: -172 } : { x: -172 }} class="absolute bg-background top-0 left-0 transition-all" style:height={portrait ? `${height}px` : '100%'} style:width={portrait ? '100%' : `${width}px`} style:box-shadow="0px 0px 20px 0px var(--color-shadow)">
-	{#key seconds}
-		{#if $t != null}
-			{@const deltaSeconds = Date.now() - $t.startDate.getTime()}
-			<div class="flex flex-col items-center gap-2 relative {$t.destination ? 'h-64' : 'h-52'} {portrait ? '' : 'top-1/2 -translate-y-1/2'}" style={`margin-${portrait ? 'top' : 'left'}: ${portrait ? Math.max(12, $safeInsets.top) : $safeInsets.top}px`}>
-				{#if $t.bikePlate}
-					<span class="font-semibold text-label text-lg">{$t.bikePlate}</span>
-				{/if}
-				<span class="text-5xl text-primary font-bold">{msToMinuteSeconds(deltaSeconds)}</span>
-				<div class="absolute top-[92px] transition-all {portrait ? $t.destination ? 'left-12' : 'left-20' : $t.destination ? 'left-4' : 'right-1/2 translate-x-1/2'}">
-					<Metric value={$t.traveledDistanceKm >= 1 ? $t.traveledDistanceKm : Math.round($t.traveledDistanceKm * 1000)} unit={$t.traveledDistanceKm >= 1 ? 'km' : 'm'} label="Distância Percorrida" />
+	{#if $t != null}
+		{@const deltaSeconds = Date.now() - $t.startDate.getTime()}
+		<div class="flex flex-col items-center gap-2 relative {$t.destination ? 'h-64' : 'h-52'} {portrait ? '' : 'top-1/2 -translate-y-1/2'}" style={`margin-${portrait ? 'top' : 'left'}: ${portrait ? Math.max(12, $safeInsets.top) : $safeInsets.top}px`}>
+			{#if $t.bikePlate}
+				<span class="font-semibold text-label text-lg">{$t.bikePlate}</span>
+			{/if}
+			<span class="text-5xl text-primary font-bold">{msToMinuteSeconds(deltaSeconds) ?? seconds}</span> <!-- Using `seconds` variable to force an update every second -->
+			<div class="absolute top-[92px] transition-all {portrait ? $t.destination ? 'left-12' : 'left-20' : $t.destination ? 'left-4' : 'right-1/2 translate-x-1/2'}">
+				<Metric value={$t.traveledDistanceKm >= 1 ? $t.traveledDistanceKm : Math.round($t.traveledDistanceKm * 1000)} unit={$t.traveledDistanceKm >= 1 ? 'km' : 'm'} label="Distância Percorrida" />
+			</div>
+			<div class="absolute transition-all {portrait ? $t.destination ? 'top-[92px] right-1/2 translate-x-1/2' : 'top-[92px] right-20 translate-x-0' : $t.destination ? 'top-[92px] right-4' : 'top-[150px] right-1/2 translate-x-1/2'}">
+				<Metric value={$t.speed < 0 ? 0 : $t.speed} unit="km/h" label="Velocidade Média" />
+			</div>
+			{#if $t.destination}
+				<div transition:fly={{ x: 64, duration: 150, easing: cubicInOut }} class="absolute transition-all {portrait ? 'top-[92px] right-12' : 'top-[150px] left-4'}">
+					{#if $t.distanceLeft != null}
+						<Metric value={$t.distanceLeft} unit="km" label="Distância em Falta" />
+					{/if}
 				</div>
-				<div class="absolute transition-all {portrait ? $t.destination ? 'top-[92px] right-1/2 translate-x-1/2' : 'top-[92px] right-20 translate-x-0' : $t.destination ? 'top-[92px] right-4' : 'top-[150px] right-1/2 translate-x-1/2'}">
-					<Metric value={$t.speed < 0 ? 0 : $t.speed} unit="km/h" label="Velocidade Média" />
-				</div>
-				{#if $t.destination}
-					<div transition:fly={{ x: 64, duration: 150, easing: cubicInOut }} class="absolute transition-all {portrait ? 'top-[92px] right-12' : 'top-[150px] left-4'}">
-						{#if $t.distanceLeft != null}
-							<Metric value={$t.distanceLeft} unit="km" label="Distância em Falta" />
+				<div transition:fade={{ duration: 150 }}>
+					<div class="absolute top-[150px] transition-all {portrait ? 'left-24' : 'right-4'}">
+						{#if $t.arrivalTime}
+							{@const timeLeft = $t.arrivalTime.getTime() - Date.now()}
+							<Metric value={msToMinuteSeconds(timeLeft)} unit="min" label="Tempo em Falta" />
 						{/if}
 					</div>
-					<div transition:fade={{ duration: 150 }}>
-						<div class="absolute top-[150px] transition-all {portrait ? 'left-24' : 'right-4'}">
-							{#if $t.arrivalTime}
-								{@const timeLeft = $t.arrivalTime.getTime() - Date.now()}
-								<Metric value={msToMinuteSeconds(timeLeft)} unit="min" label="Tempo em Falta" />
-							{/if}
-						</div>
-						<div class="absolute transition-all {portrait ? 'top-[150px] right-24' : 'top-[208px] right-1/2 translate-x-1/2'}">
-							{#if $t.arrivalTime}
-								<Metric value={formatTime($t.arrivalTime)} label="Hora de Chegada" />
-							{/if}
-						</div>
+					<div class="absolute transition-all {portrait ? 'top-[150px] right-24' : 'top-[208px] right-1/2 translate-x-1/2'}">
+						{#if $t.arrivalTime}
+							<Metric value={formatTime($t.arrivalTime)} label="Hora de Chegada" />
+						{/if}
 					</div>
-				{/if}
-			</div>
-		{/if}
-	{/key}
+				</div>
+			{/if}
+		</div>
+	{/if}
 </div>
