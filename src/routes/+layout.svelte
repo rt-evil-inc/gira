4730dd87e5affa275d4778f8ee5c6a45ce7aca8e
ttmx <git@tteles.dev>
2024-09-25 09:27:26 +0000

    Fix annoying error from using capacitor statusbar in browser

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -41,7 +41,9 @@
 		const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 		const updateTheme = () => {
 			document.documentElement.setAttribute('data-theme', $appSettings.theme === 'system' ? mediaQuery.matches ? 'dark' : 'light' : $appSettings.theme);
-			StatusBar.setStyle({ style: $appSettings.theme === 'system' ? mediaQuery.matches ? Style.Dark : Style.Light : $appSettings.theme === 'dark' ? Style.Dark : Style.Light });
+			if (Capacitor.getPlatform() === 'android' || Capacitor.getPlatform() === 'ios'){
+				StatusBar.setStyle({ style: $appSettings.theme === 'system' ? mediaQuery.matches ? Style.Dark : Style.Light : $appSettings.theme === 'dark' ? Style.Dark : Style.Light });
+			}
 		};
 		appSettings.subscribe(updateTheme);
 		mediaQuery.addEventListener('change', updateTheme);
