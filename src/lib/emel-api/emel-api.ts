bc22ae2b9334003e413a759b80458d8dc76f4124
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2024-11-09 00:47:35 +0000

    change auth API requests to use CapacitorHttp to fix CORS error

--- src/lib/emel-api/emel-api.ts
+++ src/lib/emel-api/emel-api.ts
@@ -2,47 +2,57 @@ import { firebaseToken, type Token } from '$lib/account';
 import { GIRA_AUTH_URL } from '$lib/constants';
 import type { ApiResponse, TokenOpt, UserInfo } from '$lib/emel-api/types';
 import { get } from 'svelte/store';
+import { CapacitorHttp } from '@capacitor/core';
 
 export async function getTokensLogin(email: string, password: string) {
-	return await fetch(GIRA_AUTH_URL + '/login', {
-		method: 'POST',
+	const options = {
+		url: GIRA_AUTH_URL + '/login',
 		headers: {
 			'User-Agent': 'Gira/3.2.8 (Android 34)',
 			'Content-Type': 'application/json',
 			'Priority': 'high',
 			'x-firebase-token': `${get(firebaseToken)}`,
 		},
-		body: JSON.stringify({
+		data: {
 			Provider: 'EmailPassword',
 			CredentialsEmailPassword: {
 				email,
 				password,
 			},
-		}),
-	}).then(res => res.json() as Promise<ApiResponse<TokenOpt>>);
+		},
+	};
+
+	const response = await CapacitorHttp.post(options);
+	return response.data as ApiResponse<TokenOpt>;
 }
 
 export async function getTokensRefresh(tokens: Token) {
-	return await fetch(GIRA_AUTH_URL + '/token/refresh', {
-		method: 'POST',
+	const options = {
+		url: GIRA_AUTH_URL + '/token/refresh',
 		headers: {
 			'User-Agent': 'Gira/3.2.8 (Android 34)',
 			'Content-Type': 'application/json',
 			'x-firebase-token': `${get(firebaseToken)}`,
 		},
-		body: JSON.stringify({
+		data: {
 			token: tokens.refreshToken,
-		}),
-	}).then(res => res.json() as Promise<ApiResponse<TokenOpt>>);
+		},
+	};
+
+	const response = await CapacitorHttp.post(options);
+	return response.data as ApiResponse<TokenOpt>;
 }
 
 export async function getUserInfo(tokens: Token) {
-	return await fetch(GIRA_AUTH_URL + '/user', {
-		method: 'GET',
+	const options = {
+		url: GIRA_AUTH_URL + '/user',
 		headers: {
 			'User-Agent': 'Gira/3.2.8 (Android 34)',
 			'Content-Type': 'application/json',
 			'Authorization': `Bearer ${tokens.accessToken}`,
 		},
-	}).then(res => res.json() as Promise<ApiResponse<UserInfo>>);
+	};
+
+	const response = await CapacitorHttp.get(options);
+	return response.data as ApiResponse<UserInfo>;
 }
