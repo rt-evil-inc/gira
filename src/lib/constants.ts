e4069e1643c2e6ce3713b55aae3a4de01f166c8a
Ilya Lukyanov <ilya@luk.moe>
2025-04-16 07:44:53 +0100

    update endpoints hostname
    old one is now disabled, sigh

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,8 @@
 export const LOCK_DISTANCE_m = 35;
 export const MIN_TRAVEL_DISTANCE_m = 20;
 
-export const GIRA_AUTH_URL = 'https://egira-proxy-arqetk5clq-ew.a.run.app/auth';
-export const GIRA_API_URL = 'https://egira-proxy-arqetk5clq-ew.a.run.app/api';
-export const GIRA_WS_URL = 'wss://egira-proxy-arqetk5clq-ew.a.run.app/ws';
+export const GIRA_AUTH_URL = 'https://c2g091p01.emel.pt/auth';
+export const GIRA_API_URL = 'https://c2g091p01.emel.pt/api';
+export const GIRA_WS_URL = 'wss://c2g091p01.emel.pt/ws';
 
 export const FIREBASE_TOKEN_URL = 'https://gira.rodlabs.dev/firebase-token';
